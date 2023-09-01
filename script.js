9b55ff3d7d19a7377cbbe5aa58cf8dcfbeb33bda
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-09-01 22:36:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function convertToRoman(num) {
+/*function convertToRoman(num) {
   	const obj = {
       0:['M',1000], 
       1:['D', 500], 
@@ -11,6 +11,11 @@ function convertToRoman(num) {
 
   //your code here
 
+	let romanNumerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+
+	
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
@@ -20,4 +25,39 @@ function convertToRoman(num) {
 
 
 // do not edit below this line
-module.exports = convertToRoman
+module.exports = convertToRoman */
+
+
+function decimalToRoman(num) {
+  const romanNumerals = [
+    { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
+    { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
+    { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
+    { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" },
+  ];
+
+  let result = '';
+
+  for (const { value, symbol } of romanNumerals) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
+}
+
+// Test cases
+console.log(decimalToRoman(14)); // Output: XIV
+console.log(decimalToRoman(798)); // Output: DCCXCVIII
+
