acc5cb7da1d83715774b5d950a97d7205057f308
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-09-03 16:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -27,37 +27,47 @@
 // do not edit below this line
 module.exports = convertToRoman */
 
+// Define the symbols for Roman numerals
+const romanSymbols = [
+  { value: 100000, symbol: 'C̅' },
+  { value: 10000, symbol: 'X̅' },
+  { value: 1000, symbol: 'M' },
+  { value: 900, symbol: 'CM' },
+  { value: 500, symbol: 'D' },
+  { value: 400, symbol: 'CD' },
+  { value: 100, symbol: 'C' },
+  { value: 90, symbol: 'XC' },
+  { value: 50, symbol: 'L' },
+  { value: 40, symbol: 'XL' },
+  { value: 10, symbol: 'X' },
+  { value: 9, symbol: 'IX' },
+  { value: 5, symbol: 'V' },
+  { value: 4, symbol: 'IV' },
+  { value: 1, symbol: 'I' },
+];
 
-function decimalToRoman(num) {
-  const romanNumerals = [
-    { value: 1000, symbol: "M" },
-    { value: 900, symbol: "CM" },
-    { value: 500, symbol: "D" },
-    { value: 400, symbol: "CD" },
-    { value: 100, symbol: "C" },
-    { value: 90, symbol: "XC" },
-    { value: 50, symbol: "L" },
-    { value: 40, symbol: "XL" },
-    { value: 10, symbol: "X" },
-    { value: 9, symbol: "IX" },
-    { value: 5, symbol: "V" },
-    { value: 4, symbol: "IV" },
-    { value: 1, symbol: "I" },
-  ];
+// Function to convert a number to a Roman numeral
+function convertToRoman(number) {
+  if (number <= 0 || number > 100000) {
+    return 'Invalid input';
+  }
 
   let result = '';
 
-  for (const { value, symbol } of romanNumerals) {
-    while (num >= value) {
-      result += symbol;
-      num -= value;
+  for (const symbol of romanSymbols) {
+    while (number >= symbol.value) {
+      result += symbol.symbol;
+      number -= symbol.value;
     }
   }
 
   return result;
 }
 
-// Test cases
-console.log(decimalToRoman(14)); // Output: XIV
-console.log(decimalToRoman(798)); // Output: DCCXCVIII
+// Example usage:
+const inputNumber = 798;
+const romanNumeral = convertToRoman(inputNumber);
+console.log(romanNumeral); // Output: "DCCXCVIII"
+
+
 
