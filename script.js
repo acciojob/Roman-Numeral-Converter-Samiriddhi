1a5155e67f0755841fc706311e795ce3c19356ca
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-09-04 12:55:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -25,7 +25,7 @@
 
 
 // do not edit below this line
-module.exports = convertToRoman */
+module.exports = convertToRoman 
 
 // Define the symbols for Roman numerals
 const romanSymbols = [
@@ -69,5 +69,54 @@ const inputNumber = 798;
 const romanNumeral = convertToRoman(inputNumber);
 console.log(romanNumeral); // Output: "DCCXCVIII"
 
+*/
+
+/ Function to convert a number to a Roman numeral
+function convertToRoman(num) {
+    const obj = {
+        0: ['M', 1000],
+        1: ['D', 500],
+        2: ['C', 100],
+        3: ['L', 50],
+        4: ['X', 10],
+        5: ['V', 5],
+        6: ['I', 1]
+    };
+
+    let result = '';
+
+    for (let i = 0; i < 7; i++) {
+        const [symbol, value] = obj[i];
+        const count = Math.floor(num / value);
+
+        if (count === 4 && i > 0) {
+            const prevSymbol = obj[i - 1][0];
+            result += symbol + prevSymbol;
+        } else {
+            result += symbol.repeat(count);
+        }
+
+        num %= value;
+    }
+
+    return result;
+}
+
+// Get elements from the DOM
+const numberInput = document.getElementById('numberInput');
+const convertButton = document.getElementById('convertButton');
+const resultElement = document.getElementById('result');
+
+// Attach event listener to the button
+convertButton.addEventListener('click', () => {
+    const inputValue = parseInt(numberInput.value);
+    
+    if (!isNaN(inputValue) && inputValue >= 0 && inputValue <= 100000) {
+        const romanNumeral = convertToRoman(inputValue);
+        resultElement.textContent = `Roman Numeral: ${romanNumeral}`;
+    } else {
+        resultElement.textContent = 'Please enter a valid number between 0 and 100000.';
+    }
+});
 
 
